b62458d6c833e140ae8644d47249d944998dddf9
Giorgio Gamberoni <giorgio.gamberoni@gmail.com>
2021-11-08 23:50:36 +0000

    [MINOR] Add second endpoint (#8)
    * add second endpoint
    
    * update todo
    
    * add Troubleshooting section
    
    * bump version

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,7 @@ app.listen(5000, () => {
 app.get('/url', (_req, res, _next) => {
   res.json(['hello', 'world']);
 });
+
+app.get('/lru', (_req, res, _next) => {
+  res.json(['dlrow', 'olleh']);
+});
