806eef84c4dcf79ac5f54d3335ab2263dbb63bca
king-prawns <giorgio.gamberoni@gmail.com>
2021-11-25 10:02:42 +0000

    update transport method

--- roots/src/client/createSocket.ts
+++ roots/src/client/createSocket.ts
@@ -1,7 +1,9 @@
 import {io} from 'socket.io-client';
 
 const createSocket = (proxyHost: string, port: number): void => {
-  io(`${proxyHost}:${port}`);
+  io(`${proxyHost}:${port}`, {
+    transports: ['websocket']
+  });
 };
 
 export default createSocket;
