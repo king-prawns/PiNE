cef18d51d3021b83d5c265042282c05afde74d90
Giorgio Gamberoni <giorgio.gamberoni@gmail.com>
2021-11-27 00:23:35 +0000

    add logger (#19)

---
+++ trunk/src/proxy/logger.ts
@@ -0,0 +1,6 @@
+import Logger from '../shared/logger';
+import COLOR from '../shared/logger/color';
+
+const proxyLogger = new Logger('Proxy', COLOR.Dim);
+
+export default proxyLogger;
---
+++ trunk/src/socket/logger.ts
@@ -0,0 +1,6 @@
+import Logger from '../shared/logger';
+import COLOR from '../shared/logger/color';
+
+const socketLogger = new Logger('Socket', COLOR.FgMagenta);
+
+export default socketLogger;
