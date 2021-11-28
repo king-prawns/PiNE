eaa02aca6e774ffc903768cf00391378904a080c
king-prawns <giorgio.gamberoni@gmail.com>
2021-11-28 15:46:38 +0000

    fix test

--- roots/src/getProxyManifestUrl.test.ts
+++ roots/src/getProxyManifestUrl.test.ts
@@ -3,11 +3,7 @@ import getProxyManifestUrl from './getProxyManifestUrl';
 describe('getProxyManifestUrl', () => {
   it('should create a proxy manifest url', () => {
     expect(
-      getProxyManifestUrl(
-        'http://localhost',
-        5000,
-        'http://example/manifest.mpd'
-      )
+      getProxyManifestUrl('http://localhost', 'http://example/manifest.mpd')
     ).toBe(
       'http://localhost:5000/manifest/pine.mpd?url=http://example/manifest.mpd'
     );
