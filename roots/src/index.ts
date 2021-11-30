01cdc811b53d75123f8297d9974f5666b2fbe3cf
king-prawns <giorgio.gamberoni@gmail.com>
2021-11-30 19:21:57 +0000

    add export

--- roots/src/index.ts
+++ roots/src/index.ts
@@ -1,3 +1,4 @@
 export type {default as Driver} from './interfaces/driver';
 export type {default as Proxy} from './interfaces/proxy';
 export {default as pinefy} from './pinefy';
+export {default as PlayerState} from './shared/interfaces/playerState';
