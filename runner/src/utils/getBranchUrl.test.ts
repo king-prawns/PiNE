ece61b0153088f936a7b9de0fa31b794bd4b1e0f
king-prawns <giorgio.gamberoni@gmail.com>
2022-01-16 22:13:21 +0000

    fix test

--- runner/src/utils/getBranchUrl.test.ts
+++ runner/src/utils/getBranchUrl.test.ts
@@ -2,6 +2,6 @@ import getBranchUrl from './getBranchUrl';
 
 describe('getBranchUrl', () => {
   it('should create a branch url', () => {
-    expect(getBranchUrl('http://myip')).toBe('http://myip:5000');
+    expect(getBranchUrl('http://myip')).toBe('http://myip:5001');
   });
 });
