809710ed152b68ddf62d3636dc138513edb2f149
king-prawns <giorgio.gamberoni@gmail.com>
2022-01-20 09:55:13 +0000

    add basic entry point

--- runner/src/index.ts
+++ runner/src/index.ts
@@ -1,9 +1,38 @@
-export {default as createPineRunner} from './createPineRunner';
-export {default as EAssertionType} from './enum/EAssertionType';
-export {default as EMatcher} from './enum/EMatcher';
-export {default as IClient} from './interfaces/IClient';
-export {default as IConfig} from './interfaces/IConfig';
-export {default as IRunner} from './interfaces/IRunner';
-export {default as ITestScenario} from './interfaces/ITestScenario';
-export {default as EFilter} from './shared/enum/EFilter';
-export {default as EPlayerState} from './shared/enum/EPlayerState';
+#!/usr/bin/env node
+
+import fg from 'fast-glob';
+import fs from 'fs';
+
+import logger from './logger';
+
+(async (): Promise<void> => {
+  const entries: Array<string> = await fg(['**/*.pine.json']);
+  if (entries.length === 0) {
+    logger.error(
+      'No test file found. At least one *.pine.json file should be present'
+    );
+    process.exit(1);
+  }
+
+  entries.forEach((entry: string) => {
+    const data: string = fs.readFileSync(entry, 'utf8');
+    let parsedData: any = JSON.parse(data);
+    try {
+      parsedData = JSON.parse(data);
+    } catch (e) {
+      logger.error(`Error parsing "${entry}"`);
+      logger.error(`${e}`);
+      process.exit(1);
+    }
+    // eslint-disable-next-line no-console
+    console.log(parsedData);
+    // check json schema
+    /*
+    {
+      test: 'abc',
+      test2: false,
+      test3: 44.4,
+    }
+    */
+  });
+})();
